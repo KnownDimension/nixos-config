77da7efa606a665c197dec93707f0fe763c026d1
KnownDimension <tim.heinzle@outlook.com>
2025-11-22 15:15:48 +0000

    strawberry with libgpod

--- home/programs/strawberry.nix
+++ home/programs/strawberry.nix
@@ -1,16 +1,16 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, inputs, lib, ... }:
 
 # strawberry with ipod support as original package doesnt build with it
 
 {
 
 home.packages = [
-    pkgs.strawberry.overrideAttrs = {
+    (pkgs.strawberry.overrideAttrs (oldAttrs: {
       buildInputs = oldAttrs.buildInputs ++ [pkgs.libgpod];
       cmakeFlags = [ (lib.cmakeBool "ENABLE_GPOD" true) ];
 
 
-    };
+    }))
    ];
 
 }
